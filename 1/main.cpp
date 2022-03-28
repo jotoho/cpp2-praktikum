6da664afc11301986df8e520143a1641cacc7457
Jonas Tobias Hopusch <git@jotoho.de>
2022-03-28 11:18:36 +0200

    Initial commit
    Includes our solution for the first exercise sheet (Implementation of Stack)

---
+++ 1/main.cpp
@@ -0,0 +1,19 @@
+#include "./stack.hpp"
+#include <iostream>
+
+/*
+    Tested with g++ (GCC) 11.2.0 on x86-64 Linux
+    using this command:
+    g++ -std=c++17 -Wall -Wextra -Werror -pedantic -pedantic-errors ./main.cpp ./stack.cpp
+*/
+
+int main() {
+    Stack myStack{};
+    std::cout << "Stack empty: " << myStack.is_empty() << std::endl;
+    myStack.push(8444845);
+    std::cout << "Elements in Stack: " << myStack.size() << std::endl;
+    std::cout << "Stack empty: " << myStack.is_empty() << std::endl;
+    std::cout << "Top value: " << myStack.peek() << std::endl;
+    std::cout << "Popped value: " << myStack.pop() << std::endl;
+    std::cout << "New size: " << myStack.size() << std::endl;
+}
---
+++ 1/stack.cpp
@@ -0,0 +1,45 @@
+#include "./stack.hpp"
+#include <stdexcept>
+
+Stack::Stack() {
+    this->init();
+}
+
+void Stack::init() {
+    this->next = 0;
+    this->capacity = maxSize;
+}
+
+void Stack::push(int datum) {
+    if (this->size() + 1 < this->get_capacity())
+        this->value[this->next++] = datum;
+    else
+        throw std::logic_error{"No space left inside Stack"};
+}
+
+int Stack::pop() {
+    if (this->is_not_empty())
+        return this->value[--(this->next)];
+    else
+        throw std::logic_error{"Can't take something from the stack if it's empty"};
+}
+
+int Stack::peek() {
+    return this->value[this->next - 1];
+}
+
+int Stack::size() {
+    return this->next;
+}
+
+int Stack::get_capacity() {
+    return Stack::maxSize;
+}
+
+bool Stack::is_empty() {
+    return this->next == 0;
+}
+
+bool Stack::is_not_empty() {
+    return this->next != 0;
+}
