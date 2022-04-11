4427ad65c96a8be13936b06c93fa762871f57531
Tim Beckmann <80764079+TimBeckmann@users.noreply.github.com>
2022-04-11 12:00:28 +0200

    Modified Stack to use dynamically alloc memory

--- 2/stack.cpp
+++ 2/stack.cpp
@@ -1,34 +1,88 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Repository: https://github.com/jotoho/cpp2-praktikum
 #include "./stack.hpp"
+#include <cstring>
+#include <memory>
 #include <stdexcept>
 
 Stack::Stack() {
     this->init();
 }
 
+Stack::Stack(const Stack& other) {
+    *this = other;
+}
+
+Stack::~Stack() {
+    std::free(this->value);
+}
+
+const Stack& Stack::operator=(const Stack& other) {
+    if (this == &other)
+        return *this;
+
+    this->currentCapacity = other.currentCapacity;
+    this->next = other.next;
+    if (other.value == nullptr) {
+        this->value = nullptr;
+    } else {
+        this->value =
+            reinterpret_cast<int*>(malloc(sizeof(int) * this->currentCapacity));
+        if (this->value == nullptr)
+            throw std::bad_alloc{};
+        else
+            std::memcpy(this->value, other.value,
+                        currentCapacity * sizeof(int));
+    }
+    return *this;
+}
+
 void Stack::init() {
+    if (value != nullptr) {
+        std::free(this->value);
+    }
     this->next = 0;
-    this->capacity = maxSize;
+    this->currentCapacity = Stack::STARTING_CAPACITY;
+    this->value =
+        reinterpret_cast<int*>(malloc(sizeof(int) * Stack::STARTING_CAPACITY));
+    if (this->value == nullptr) {
+        throw std::bad_alloc{};
+    }
 }
 
 void Stack::push(int datum) {
-    if (this->size() + 1 < this->get_capacity())
-        this->value[this->next++] = datum;
-    else
-        throw std::logic_error{"No space left inside Stack"};
+    while (this->next >= this->currentCapacity - 1) {
+        this->currentCapacity += Stack::INCREMENT;
+        this->value = reinterpret_cast<int*>(
+            std::realloc(this->value, this->currentCapacity));
+        if (this->value == nullptr) {
+            throw std::bad_alloc{};
+        }
+    }
+    this->value[this->next++] = datum;
 }
 
 int Stack::pop() {
-    if (this->is_not_empty())
-        return this->value[--(this->next)];
-    else
-        throw std::logic_error{
-            "Can't take something from the stack if it's empty"};
+    if (is_not_empty()) {
+        const auto result = this->value[--this->next];
+        while (this->next >= Stack::STARTING_CAPACITY &&
+               this->next + 1 <= this->currentCapacity - Stack::INCREMENT) {
+            this->currentCapacity -= Stack::INCREMENT;
+            this->value = reinterpret_cast<int*>(
+                std::realloc(this->value, this->currentCapacity));
+            if (this->value == nullptr)
+                throw std::bad_alloc{};
+        }
+        return result;
+    } else
+        throw std::underflow_error{"Tried to pop from empty stack!"};
 }
 
 int Stack::peek() {
-    return this->value[this->next - 1];
+    if (is_not_empty())
+        return this->value[this->next - 1];
+    else
+        throw std::underflow_error{"Tried to peek at empty stack!"};
 }
 
 int Stack::size() {
@@ -36,7 +90,7 @@ int Stack::size() {
 }
 
 int Stack::get_capacity() {
-    return Stack::maxSize;
+    return this->currentCapacity;
 }
 
 bool Stack::is_empty() {
