68ad00ac9861e173fea9ceeb1e2cd8add5039339
Jonas Tobias Hopusch <git@jotoho.de>
2022-04-20 16:36:07 +0200

    Implement double linked list as described in on sheet 3

---
+++ 3/list.cpp
@@ -0,0 +1,37 @@
+#include "./list.hpp"
+
+// Voll moppelkotze - sollte append heißen
+void Liste::add(int wert) {
+    auto oldtail = tail;
+    tail =
+        new Liste::Element(wert, tail, static_cast<Liste::Element*>(nullptr));
+    if (oldtail != nullptr)
+        oldtail->next = tail;
+    else
+        head = tail;
+}
+
+void Liste::clear() {
+    while (head != nullptr) {
+        auto oldhead = head;
+        head = head->next;
+        delete oldhead;
+    }
+    tail = nullptr;
+}
+
+bool Liste::contains(int wert) {
+    auto currentElement = head;
+    while (head != nullptr)
+        if (currentElement->wert == wert)
+            return true;
+        else
+            currentElement = currentElement->next;
+    return false;
+}
+
+Liste::Liste() {}
+
+Liste::~Liste() {
+    this->clear();
+}
