0faafc00901646a497679cab5073691810ba82d0
Jonas Tobias Hopusch <git@jotoho.de>
2022-06-13 11:36:17 +0200

    Improve inspectability of testing results

--- 8/main.cpp
+++ 8/main.cpp
@@ -2,23 +2,33 @@
 // Repository: https://github.com/jotoho/cpp2-praktikum
 
 #include <cstdlib>
+#include <iostream>
 #include "./fraction.cpp"
+
+template <typename T>
+requires std::is_integral_v<T> && std::is_signed_v<T>
+std::ostream& operator<<(
+    std::ostream& out,
+    const Fraction<T>& val) {
+    out << "Fraction{" << val.getNominator() << ", " << val.getDenominator()
+        << '}';
+    return out;
+}
 
 class TestFractions {
     TestFractions() = delete;
 
    public:
-    static void testConstructing() {
-        {
-            [[maybe_unused]] Fraction a{5, 5};
-            [[maybe_unused]] Fraction b{10, 2};
-            [[maybe_unused]] Fraction c{15, 16};
-        }
+    static void testConstructing(std::ostream& out) {
+        out << "{5,5}: " << Fraction{5, 5} << '\n';
+        out << "{10,2}: " << Fraction{10, 2} << '\n';
+        out << "{15,16}: " << Fraction{15, 16} << '\n';
 
         {
             bool caughtIt = false;
             try {
-                [[maybe_unused]] Fraction d{99, 0};
+                Fraction invalid{99, 0};
+                out << "This fraction is illegal: " << invalid << '\n';
             } catch (const std::invalid_argument& e) {
                 caughtIt = true;
             }
@@ -29,14 +39,14 @@ class TestFractions {
         }
     }
 
-    static void testOperators() {
-        Fraction{3, 6} + Fraction{7, 10};
-        Fraction{9, 4} + Fraction{13, 7};
+    static void testOperators(std::ostream& out) {
+        out << "3/6 + 7/10 = " << (Fraction{3, 6} + Fraction{7, 10}) << '\n';
+        out << "9/4 + 13/7 = " << (Fraction{9, 4} + Fraction{13, 7}) << '\n';
     }
 };
 
 int main() {
-    TestFractions::testConstructing();
-    TestFractions::testOperators();
+    TestFractions::testConstructing(std::cout);
+    TestFractions::testOperators(std::cout);
     return EXIT_SUCCESS;
 }
